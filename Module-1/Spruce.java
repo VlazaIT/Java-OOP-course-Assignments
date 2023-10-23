ca5a42adba51a6c18da0d141e685593218ff2a54
Vlad Zakatov <vladislav.zakatov@metropolia.fi>
2023-10-23 16:00:56 +0300

    Added Task 2 for Module 1.1

---
+++ Module-1/Spruce.java
@@ -0,0 +1,9 @@
+// Module 1.1. Task 2
+public class Spruce {
+    public static void main (String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
